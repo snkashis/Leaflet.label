7781d31ce7e514b52c759e45e96d6a4c0cbfaf2e
snkashis <steve@studiosanderson.com>
2013-01-23 17:49:20 -0500

    refine a bit

--- src/Map.Label.js
+++ src/Map.Label.js
@@ -1,6 +1,7 @@
 L.Map.include({
 	showLabel: function (label) {
-		if (label._source.options.opacity && label._source.options.opacity === 0) {return; }
+		if (label._source.options.opacity === 0) {return; }
+		
 		this._label = label;
 
 		return this.addLayer(label);
