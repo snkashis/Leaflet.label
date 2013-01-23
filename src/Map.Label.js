03b56409beef894dac5686c35faacef68748e356
snkashis <steve@studiosanderson.com>
2013-01-23 17:37:47 -0500

    If the entity holding label has an opacity of 0, don't show the label.

--- src/Map.Label.js
+++ src/Map.Label.js
@@ -1,5 +1,6 @@
 L.Map.include({
 	showLabel: function (label) {
+		if (label._source.options.opacity && label._source.options.opacity === 0) {return; }
 		this._label = label;
 
 		return this.addLayer(label);
